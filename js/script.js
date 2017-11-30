ee992471779ee1fdccc808bbf25d1707880af4a0
cotrutatiberiu <32967624+cotrutatiberiu@users.noreply.github.com>
2017-11-30 18:21:21 +0200

    navbardn

--- js/script.js
+++ js/script.js
@@ -1,74 +1,82 @@
+var workE = document.getElementById("workElem");
+var contactE = document.getElementById("contactElem");
 var homeE = document.getElementById("homeElem");
-var aboutE=document.getElementById("aboutElem");
-var workE=document.getElementById("workElem");
-var contactE=document.getElementById("contactElem");
-var right = 200;
-var left = 0;
-var define=20;
+var aboutE = document.getElementById("aboutElem");
+
+//Move home element
 function moveHome() {
+  var moveRightHome = parseInt(homeE.style.right, 10);
+  var moveLeftHome = 0;
+  var definePositionHome = 20;
+
   var moveElemRight = setInterval(minus, 1);
   function minus() {
-    if (right == -20) {
+    if (moveRightHome == -20) {
       clearInterval(moveElemRight);
       var moveElemLeft = setInterval(function() {
-        if (left == 20) {
+        if (moveLeftHome == 20) {
           clearInterval(moveElemLeft);
-          var defineElem=setInterval(function(){
-            if(define==0){
-                clearInterval(defineElem);
-            }else{
-                define--;
-                homeE.style.right=define+"px";
+          var definePositionHomeElem = setInterval(function() {
+            if (definePositionHome == 0) {
+              clearInterval(definePositionHomeElem);
+            } else {
+              definePositionHome--;
+              homeE.style.right = definePositionHome + "px";
             }
-          },10)
+          }, 10);
         } else {
-          left++;
-          homeE.style.right = left + "px";
+          moveLeftHome++;
+          homeE.style.right = moveLeftHome + "px";
         }
       }, 10);
     } else {
-      right--;
-      homeE.style.right = right + "px";
+      moveRightHome--;
+      homeE.style.right = moveRightHome + "px";
     }
   }
 }
 
-function fadeIn(element,speed){
-    var fadeInElem=setInterval(function(){
-        if(element.style.opacity==1){
-            clearInterval(fadeInElem);
-        }else{
-        element.style.opacity=Number(element.style.opacity)+0.2;
-        }
-    },speed)
-}
-
-function moveUpDown(element,speed,way){
-    var x=parseInt(homeE.style.top,10);
-    switch(way){
-        case "up":
-        break;
-        case "down":
-        var goDown=setInterval(function(){
-            if(x==0){
-                clearInterval(goDown);
-            }else{
-                x--;
-                element.style.top=x +"px";
-            }
-        },speed)
-        break;
+//Fade in element
+function fadeIn(element, speed) {
+  var fadeInElem = setInterval(function() {
+    if (element.style.opacity == 1) {
+      clearInterval(fadeInElem);
+    } else {
+      element.style.opacity = Number(element.style.opacity) + 0.2;
     }
+  }, speed);
 }
 
+//Move up/down elements
+function moveUpDown(element, speed, way) {
+  var x = parseInt(element.style.top, 10);
+  var y= parseInt(element.style.bottom,10)
+  switch (way) {
+    case "up":
+      var goUp = setInterval(function() {
+        if (x == 0) {
+          clearInterval(goUp);
+        } else {
+          x--;
+          element.style.top = x + "px";
+        }
+      }, speed);
+      break;
+    case "down":
+      var goDown = setInterval(function() {
+        if (y == 0) {
+          clearInterval(goDown);
+        } else {
+          y--;
+          element.style.bottom = y + "px";
+        }
+      }, speed);
+      break;
+  }
+}
 
+//Calls
 moveHome();
-fadeIn(homeE,100);
-// moveUpDown(aboutE,1,"down");
-
-
-function test(){
-    var a=aboutE.style.top[2];
-    console.log(a);
-}
-test();
+fadeIn(homeE, 100);
+moveUpDown(aboutE, 1, "up");
+moveUpDown(contactE,1,"down");
