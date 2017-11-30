f3857635999f8018e2bea00f7e442fa55a402083
cotrutatiberiu <32967624+cotrutatiberiu@users.noreply.github.com>
2017-11-30 16:31:09 +0200

    up

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 var homeE = document.getElementById("homeElem");
-var time = 1;
-var caze = false;
 var right = 300;
+var left=0;
 function moveRight() {
     var moveElemRight = setInterval(minus, 10);
     function minus() {
         if (right == 0) {
             clearInterval(moveElemRight);
+            setInterval(function(){
+                left++;
+                homeE.style.right=left+"px";
+            },10)
         } else {
             right--;
             homeE.style.right = right + "px";
         }
     }
-
-
-
 }
 
 moveRight();
